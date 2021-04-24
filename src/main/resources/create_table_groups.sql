445f1a85a469cd1b7f0da00208b75437eebd563d
Ivan Ivanov <ii99xt1@gmail.com>
2021-04-24 09:01:45 +0300

    change type id from bigint to int in scripts create tables

--- src/main/resources/create_table_groups.sql
+++ src/main/resources/create_table_groups.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE public.groups
 (
-    group_id bigint NOT NULL DEFAULT nextval('groups_group_id_seq'::regclass),
+    group_id integer NOT NULL DEFAULT nextval('groups_group_id_seq'::regclass),
     group_name character varying(10) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT groups_pkey PRIMARY KEY (group_id)
 )
--- src/main/resources/create_table_students.sql
+++ src/main/resources/create_table_students.sql
@@ -4,8 +4,8 @@
 
 CREATE TABLE public.students
 (
-    student_id bigint NOT NULL DEFAULT nextval('students_student_id_seq'::regclass),
-    group_id bigint,
+    student_id integer NOT NULL DEFAULT nextval('students_student_id_seq'::regclass),
+    group_id integer,
     first_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
     last_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT students_pkey PRIMARY KEY (student_id),
