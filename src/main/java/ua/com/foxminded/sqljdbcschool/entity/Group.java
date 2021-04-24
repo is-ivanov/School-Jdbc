02ef44c77df70b0d370e6c448d39240422eb513d
Ivan Ivanov <ii99xt1@gmail.com>
2021-04-24 14:24:34 +0300

    change constructor in Group class

--- src/main/java/ua/com/foxminded/sqljdbcschool/entity/Group.java
+++ src/main/java/ua/com/foxminded/sqljdbcschool/entity/Group.java
@@ -4,7 +4,9 @@ public class Group {
     private int groupId;
     private String groupName;
     
-    public Group() {
+    public Group(int groupId, String groupName) {
+        this.groupId = groupId;
+        this.groupName = groupName;
     }
 
     public String getGroupName() {
