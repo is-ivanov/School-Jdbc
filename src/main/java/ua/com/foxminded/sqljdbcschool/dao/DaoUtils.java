366e79ad5473714850e91ce2113cb6185213343d
Ivan Ivanov <ii99xt1@gmail.com>
2021-04-24 14:23:49 +0300

    create getConnaction method in daoUtils

---
+++ src/main/java/ua/com/foxminded/sqljdbcschool/dao/DaoUtils.java
@@ -0,0 +1,25 @@
+package ua.com.foxminded.sqljdbcschool.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DaoUtils {
+    private static final String DATABASE_URL = "jdbc:posgresql://localhost:5432/school";
+    private static final String USER = "karyama";
+    private static final String PASSWORD = "1234";
+    private static final String MESSAGE_CONNECTION_ERROR = "Can't create connection";
+
+    Connection getConnection() throws DAOException {
+        Connection connection = null;
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            connection = DriverManager.getConnection(DATABASE_URL, USER,
+                    PASSWORD);
+        } catch (SQLException e) {
+            throw new DAOException(MESSAGE_CONNECTION_ERROR, e);
+        }
+        return connection;
+
+    }
+}
