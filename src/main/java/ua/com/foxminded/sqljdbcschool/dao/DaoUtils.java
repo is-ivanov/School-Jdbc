8fc2091bc00f3684d9b239767b6360fd7cc6b298
Ivan Ivanov <ii99xt1@gmail.com>
2021-04-27 23:08:29 +0300

    extract constants

--- src/main/java/ua/com/foxminded/sqljdbcschool/dao/DaoUtils.java
+++ src/main/java/ua/com/foxminded/sqljdbcschool/dao/DaoUtils.java
@@ -9,6 +9,9 @@ import ua.com.foxminded.sqljdbcschool.exception.DAOException;
 import ua.com.foxminded.sqljdbcschool.reader.Reader;
 
 public class DaoUtils {
+    private static final String DB_PASSWORD = "db.password";
+    private static final String DB_LOGIN = "db.login";
+    private static final String DB_URL = "db.url";
     private static final String FILENAME_DB_PROPERTIES = "db.properties";
     private static final String MESSAGE_CONNECTION_ERROR = "Can't create connection";
 
@@ -20,9 +23,9 @@ public class DaoUtils {
         
         try {
             DriverManager.registerDriver(new org.postgresql.Driver());
-            connection = DriverManager.getConnection(properties.getProperty("db_url"),
-                    properties.getProperty("login"),
-                    properties.getProperty("password"));
+            connection = DriverManager.getConnection(properties.getProperty(DB_URL),
+                    properties.getProperty(DB_LOGIN),
+                    properties.getProperty(DB_PASSWORD));
         } catch (SQLException e) {
             throw new DAOException(MESSAGE_CONNECTION_ERROR, e);
         }
