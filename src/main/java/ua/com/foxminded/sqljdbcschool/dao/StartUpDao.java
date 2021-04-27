6a8a0605022942c5238ed756cc967bbeb7005dbc
Ivan Ivanov <ii99xt1@gmail.com>
2021-04-28 00:12:32 +0300

    change properties files and StartUp class

--- src/main/java/ua/com/foxminded/sqljdbcschool/dao/StartUpDao.java
+++ src/main/java/ua/com/foxminded/sqljdbcschool/dao/StartUpDao.java
@@ -4,20 +4,25 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.*;
+import java.util.Properties;
 
 import org.apache.ibatis.jdbc.ScriptRunner;
 
 import ua.com.foxminded.sqljdbcschool.exception.DAOException;
+import ua.com.foxminded.sqljdbcschool.reader.Reader;
 
 public class StartUpDao {
-
+    
+    private static final String PROPERTY_SQL_DELETE_TABLES = "tables.delete.ifexist";
+    private static final String FILENAME_SQL_QUERY_PROPERTIES = "sql_query.properties";
     private DaoUtils daoUtil = new DaoUtils();
 
     public void deleteTables() throws DAOException {
-        String sqlCommands = "DROP TABLE IF EXISTS groups CASCADE; "
-                + "DROP TABLE IF EXISTS students CASCADE; "
-                + "DROP TABLE IF EXISTS courses CASCADE; "
-                + "DROP TABLE IF EXISTS students_courses CASCADE";
+        Reader reader = new Reader();
+        Properties properties = reader.readProperties(FILENAME_SQL_QUERY_PROPERTIES);
+        
+        
+        String sqlCommands = properties.getProperty(PROPERTY_SQL_DELETE_TABLES);
         String[] sql = sqlCommands.split(";");
 
         try (Connection connection = daoUtil.getConnection()) {
