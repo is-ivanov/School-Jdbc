a8baf5d80594fe732a9a9817c82ce819529cff49
DESKTOP-U2527QI\mnpz <h2h.eredivisie@gmail.com>
2021-04-28 08:07:11 +0300

    extract constant in Reader class

--- src/main/java/ua/com/foxminded/sqljdbcschool/reader/Reader.java
+++ src/main/java/ua/com/foxminded/sqljdbcschool/reader/Reader.java
@@ -6,17 +6,19 @@ import java.util.Properties;
 
 public class Reader {
 
+    private static final String MASK_MESSAGE_FILE_NOT_FOUND = "File %s not found";
+
     public Properties readProperties(String filename) {
         Properties properties = new Properties();
-        
+
         try (InputStream input = getClass().getClassLoader()
                 .getResourceAsStream(filename)) {
-            
+
             properties.load(input);
         } catch (IOException e) {
-            System.err
-            .println(String.format("File %s not found", filename));
-            
+            System.err.println(
+                    String.format(MASK_MESSAGE_FILE_NOT_FOUND, filename));
+
         }
         return properties;
     }
