99d09841ff1ac750a2e33b62c7fc17b0f881b539
DESKTOP-U2527QI\mnpz <h2h.eredivisie@gmail.com>
2021-04-28 14:11:44 +0300

    add method readTxtDataFiles

--- src/main/java/ua/com/foxminded/sqljdbcschool/reader/Reader.java
+++ src/main/java/ua/com/foxminded/sqljdbcschool/reader/Reader.java
@@ -1,8 +1,13 @@
 package ua.com.foxminded.sqljdbcschool.reader;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 public class Reader {
 
@@ -21,5 +26,23 @@ public class Reader {
 
         }
         return properties;
+    }
+
+    public List<String> readTxtDataFiles(String filename) {
+        List<String> fileContents = new ArrayList<>();
+        try (InputStream inputStream = getClass().getClassLoader()
+                .getResourceAsStream(filename);
+                InputStreamReader inputStreamReader = new InputStreamReader(
+                        inputStream);
+                BufferedReader bufferedReader = new BufferedReader(
+                        inputStreamReader);) {
+            fileContents = bufferedReader.lines().collect(Collectors.toList());
+
+        } catch (IOException | NullPointerException e) {
+            System.err.println(
+                    String.format(MASK_MESSAGE_FILE_NOT_FOUND, filename));
+        }
+        return fileContents;
+
     }
 }
