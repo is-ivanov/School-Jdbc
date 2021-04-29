a4b7fa4f7ebd82fa950c2b3bf6f57462f1542b78
Ivan Ivanou <ii99xt1@gmail.com>
2021-04-30 00:05:15 +0300

    add menu.properties

--- src/main/java/ua/com/foxminded/sqljdbcschool/ui/Menu.java
+++ src/main/java/ua/com/foxminded/sqljdbcschool/ui/Menu.java
@@ -1,10 +1,25 @@
 package ua.com.foxminded.sqljdbcschool.ui;
 
+import java.util.Properties;
+import java.util.Scanner;
+
+import ua.com.foxminded.sqljdbcschool.reader.Reader;
+
 public class Menu {
+    private static final String FILENAME_MENU_PROPERTIES = "menu.properties";
 
-    public static int menu() {
-        int selection = 0;
+    private Reader reader = new Reader();
+    private Properties propMenu = reader.readProperties(FILENAME_MENU_PROPERTIES);
 
+    public int menu() {
+        int selection;
+        String menuFirstLevel = propMenu.getProperty("menu.level1");
+        System.out.println(menuFirstLevel);
+        Scanner input = new Scanner(System.in);
+        selection = input.nextInt();
+        input.close();
+        
+        
         return selection;
     }
 }
