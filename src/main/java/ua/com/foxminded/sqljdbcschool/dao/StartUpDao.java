24ce0fcca20cfba2825415e71b0e03cebaa15e57
Ivan Ivanov <ii99xt1@gmail.com>
2021-05-07 00:18:46 +0300

    extract method sqlscriptrunner into class

--- src/main/java/ua/com/foxminded/sqljdbcschool/dao/StartUpDao.java
+++ src/main/java/ua/com/foxminded/sqljdbcschool/dao/StartUpDao.java
@@ -1,12 +1,7 @@
 package ua.com.foxminded.sqljdbcschool.dao;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.sql.*;
 import java.util.Properties;
-
-import org.apache.ibatis.jdbc.ScriptRunner;
 
 import ua.com.foxminded.sqljdbcschool.exception.DAOException;
 import ua.com.foxminded.sqljdbcschool.reader.Reader;
@@ -16,18 +11,13 @@ public class StartUpDao {
     private static final String PROPERTY_SQL_DELETE_TABLES = "tables.delete.ifexist";
     private static final String FILENAME_SQL_QUERY_PROPERTIES = "sql_query.properties";
     private static final String MESSAGE_EXCEPTION = "Can't run script";
-    private static final String FORMAT_MASK_MESSAGE_EXCEPTION = "Script %s not found!!";
     private static final String FILENAME_SCRIPT_CREATE_TABLES = "create_all_tables.sql";
 
-    private DaoUtils daoUtil = new DaoUtils();
-
-    
     public void prepareTables() throws DAOException {
         deleteTables();
         createTables(FILENAME_SCRIPT_CREATE_TABLES);
     }
-    
-    
+
     private void deleteTables() throws DAOException {
         Reader reader = new Reader();
         Properties properties = reader
@@ -36,7 +26,7 @@ public class StartUpDao {
         String sqlCommands = properties.getProperty(PROPERTY_SQL_DELETE_TABLES);
         String[] sql = sqlCommands.split(DELIMITER_QUERIES);
 
-        try (Connection connection = daoUtil.getConnection()) {
+        try (Connection connection = DaoUtils.getConnection()) {
             try (Statement statement = connection.createStatement()) {
 
                 connection.setAutoCommit(false);
@@ -50,24 +40,13 @@ public class StartUpDao {
             throw new DAOException();
         }
     }
-    
+
     private void createTables(String scriptFilename) throws DAOException {
-        DaoUtils daoUtils = new DaoUtils();
 
-        try (Connection connection = daoUtils.getConnection()) {
+        try (Connection connection = DaoUtils.getConnection()) {
 
-            ScriptRunner scriptRunner = null;
-
-            scriptRunner = new ScriptRunner(connection);
-            try (InputStream inputStream = getClass().getClassLoader()
-                    .getResourceAsStream(scriptFilename);
-                    InputStreamReader reader = new InputStreamReader(
-                            inputStream);) {
-                scriptRunner.runScript(reader);
-            } catch (IOException | NullPointerException e) {
-                throw new DAOException(String.format(
-                        FORMAT_MASK_MESSAGE_EXCEPTION, scriptFilename), e);
-            }
+            SqlScriptRunner scriptRunner = new SqlScriptRunner(connection);
+            scriptRunner.runSqlScript(scriptFilename);
 
         } catch (SQLException e) {
             throw new DAOException(MESSAGE_EXCEPTION, e);
